abde4cb8d278f9f5edfd247db7e2ee2d0b28aa89
sergey <bolszow@gmail.com>
2015-05-28 23:08:25 +0200

    install gulp modules and create jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,33 @@
 var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var angularFilesort = require('gulp-angular-filesort');
+var jshint = require('gulp-jshint');
+var notify = require('gulp-notify');
 
-gulp.task('test', function() {
-	console.log('test task works!');
+var onError = function(err) {
+    console.log(err);
+};
+
+gulp.task('jshint', function() {
+    return gulp.src(['./app/*.js', './app/*/*.js'])
+        .pipe(plumber({
+            errorHandler: onError
+        }))
+        .pipe(jshint())
+        .pipe(notify(function(file) {
+
+            if (file.jshint.success) {
+                // Don't show something if success
+                return false;
+            }
+
+            var errors = file.jshint.results.map(function(data) {
+                if (data.error) {
+                    return "(" + data.error.line + ':' + data.error.character + ') ' + data.error.reason;
+                }
+            }).join("\n");
+
+            return file.relative + " (" + file.jshint.results.length + " errors)\n" + errors;
+        }));
 });
