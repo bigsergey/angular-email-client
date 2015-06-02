7715edca77ea73e2485063af46f1980d20cdeb74
Sergey Bolshov <bolszow@gmail.com>
2015-06-03 00:25:59 +0200

    simple autocomplete directive

--- app/search/search.js
+++ app/search/search.js
@@ -1,4 +1,18 @@
 angular.module('angularEmailClient.search', [])
+    .directive('autocomnpete', function() {
+        return {
+            restrict: 'E',
+            replace: true,
+            scope: {
+                tags: '=',
+                currentValue: '='
+            },
+            templateUrl: './app/search/autocomplete.html',
+            link: function(scope, element, attrs) {
+
+            }
+        }
+    })
     .directive('search', function() {
         return {
             restrict: 'E',
@@ -7,7 +21,18 @@ angular.module('angularEmailClient.search', [])
             },
             templateUrl: './app/search/searchView.html',
             link: function(scope, element, attrs) {
+                scope.searchHistory = []
+                element.bind("keydown keypress", function(event) {
+                    if (event.which === 13) {
+                        if (scope.searchHistory.indexOf(scope.bindModel)) {
+                            scope.$apply(function() {
+                                scope.searchHistory.push(scope.bindModel);
+                            });
+                        }
 
+                        event.preventDefault();
+                    }
+                });
             }
         };
     });
